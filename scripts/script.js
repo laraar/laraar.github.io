4e3481feed5ba49fcf5fbfa680aca3194ff4209c
laraar <64051745+laraar@users.noreply.github.com>
2025-04-20 18:18:04 -0500

    Add files via upload
    Contact form and menu scripts

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,17 @@ document.getElementById("logo").addEventListener("click", function () {
   const tv = document.getElementById("style-examples");
   tv.style.display = tv.style.display === "block" ? "none" : "block";
 });
+
+const toggleMenuBtn = document.getElementById("menu-button");
+const menu = document.getElementById("dropdown-menu");
+toggleMenuBtn.addEventListener("click", () => {
+  menu.classList.toggle("visible");
+  menu.classList.toggle("hidden");
+});
+
+const toggleContactForm = document.getElementById("contact-button");
+const form = document.getElementById("contact-form");
+toggleContactForm.addEventListener("click", () => {
+  form.classList.toggle("visible");
+  form.classList.toggle("hidden");
+});
